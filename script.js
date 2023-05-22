8d6ca4ba3c90b1b337eae9bc34885bb1fa03373a
xRitu <134009855+Ritubandod@users.noreply.github.com>
2023-05-23 01:15:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+        // JavaScript to hide all content inside the body and show custom content
+        window.onload = function() {
+            // Hide all elements inside the body
+            var bodyElements = document.querySelectorAll('body > *');
+            for (var i = 0; i < bodyElements.length; i++) {
+                bodyElements[i].style.display = 'none';
+            }
+
+            // Create the custom content
+            var customContent = document.createElement('div');
+            customContent.id = 'custom-content';
+            customContent.innerHTML = '<h1>Welcome to Custom HTML Content!</h1><p>This is a sample text.</p>';
+
+            // Append the custom content to the body
+            document.body.appendChild(customContent);
+        };
